fb9f95867aa6eee461589000d4ef9a37c219d80c
Jia Can <canjia.mail@gmail.com>
2018-01-14 14:38:04 +0000

    first follow sucess

--- src/xiaochetest/Definition.h
+++ src/xiaochetest/Definition.h
@@ -10,126 +10,173 @@
 #define bool int  // define type bool
 
 // define some values
-uint8_t spd=200;
-uint8_t tim=20;
-char command;
+uint8_t g_spd=200;  // g for global
+uint8_t g_turn_spd = 150;
+uint8_t g_tim=1; //1000 ms
+uint8_t g_turn_tim = 1; //100 ms
+char command = ' ';
+bool alarm = 0;
+bool danger = 0;
+//long l_randNumer = -1; // for random run
+
+
 
 // initialize some instances
 AF_DCMotor motoryou(3);//右轮马达接到m3上
 AF_DCMotor motorzuo(4);//左轮马达接到m4上
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);// NewPing setup of pins
  
+ 
+ 
+ 
+ 
 // define function header
+
+//// setup 
+void setup_serial();
+void setup_motors();
+//// movement
 void forward(uint8_t,uint8_t);//前进函数
 void backward(uint8_t,uint8_t);//后退函数
 void leftturn(uint8_t,uint8_t);//左转函数
 void rightturn(uint8_t,uint8_t);//右转函数
 void nowstop(uint8_t);//刹车即不动函数
-void random_run(uint8_t, uint8_t); //random run without command from raspberrypi
+void random_run(); //random run without command from raspberrypi
+void go_home(char p_command);
+//// sensor 
 bool is_danger(); // if puppy is too close to something
 bool is_alarm();  // if people is close.
-void print_sonar();
+//// communication
 char get_command();
-void random_run();
-void setup_serial();
-void setup_motors();
+//// debug
+void print_sonar();
+void print_human_sensor();
+
+
+
+
 
 // funciton body
+
+//// setup
 void setup_serial(){
-  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.begin(9600);           
   Serial.println("Motor test!");
 }
 void setup_motors(){
-    // turn on motor
   motorzuo.setSpeed(200);
   motoryou.setSpeed(200);
   motorzuo.run(RELEASE);
   motoryou.run(RELEASE);
 }
-void forward(uint8_t spd=200,uint8_t tim=20){
-  if(is_danger()){
-    leftturn(spd, tim);
-    Serial.println("danger! turn left");
-  }
-  else{
-    Serial.println("No danger, move forward");
-    motorzuo.run(BACKWARD);
-    motoryou.run(BACKWARD); 
-    motorzuo.setSpeed(spd);    
-    motoryou.setSpeed(spd); 
-    delay(100*tim);
-  }
+
+//// movements
+void forward(uint8_t p_spd=200,uint8_t p_tim=20){ // p for paremeter
+    //Serial.println("No danger, move forward");
+  motorzuo.run(BACKWARD);
+  motoryou.run(BACKWARD); 
+  motorzuo.setSpeed(p_spd + 50);   // 10 meets upl left speed slow 
+  motoryou.setSpeed(p_spd); 
+  delay(100*p_tim);
 }
-void backward(uint8_t spd=200,uint8_t tim=20){
+void backward(uint8_t p_spd=200,uint8_t p_tim=20){
   motorzuo.run(FORWARD);
   motoryou.run(FORWARD);
-  motorzuo.setSpeed(spd);    
-  motoryou.setSpeed(spd); 
-  delay(100*tim);
+  motorzuo.setSpeed(p_spd);    
+  motoryou.setSpeed(p_spd); 
+  delay(100*p_tim);
 }
-void leftturn(uint8_t spd=200,uint8_t tim=20){
+void leftturn(uint8_t p_turn_spd=200,uint8_t p_tim=20){
   motorzuo.run(FORWARD);
   motoryou.run(BACKWARD); 
-  motorzuo.setSpeed(spd);    
-  motoryou.setSpeed(spd); 
-  delay(100*tim);
+  motorzuo.setSpeed(p_turn_spd);    
+  motoryou.setSpeed(p_turn_spd); 
+  delay(100*p_tim);
 }
-
-void rightturn(uint8_t spd=200,uint8_t tim=20){
+void rightturn(uint8_t p_turn_spd=200,uint8_t p_tim=20){
   motoryou.run(FORWARD);
   motorzuo.run(BACKWARD); 
-  motorzuo.setSpeed(spd);    
-  motoryou.setSpeed(spd); 
-  delay(100*tim);
+  motorzuo.setSpeed(p_turn_spd);    
+  motoryou.setSpeed(p_turn_spd); 
+  delay(100*p_tim);
 }
-
-void nowstop(uint8_t tim=20){
+void nowstop(uint8_t p_tim=20){
   motorzuo.run(RELEASE);
   motoryou.run(RELEASE); 
-  delay(100*tim);
+  delay(100*p_tim);
+}
+void random_run(){
+  randomSeed(digitalRead(10
+  ));
+  /* get a random num (0, 1, 2, 3, 4)
+  switch case(num)
+  0 for stop
+  1 for forward
+  2 for leftturn
+  3 for rightturn
+  4 for backward  */
+  long l_randNumber = random(0,7); // l for local
+  //Serial.print("Random number: ");
+  //Serial.println(l_randNumber);
+  long l_rand_spd = random(100, 300);
+  long l_rand_tim = random(0, 20);
+  Serial.println(l_rand_tim);
+  Serial.println(l_rand_spd);
+  switch(l_randNumber){
+    case(0):{nowstop(); Serial.println("random num 0, stop");break;}  
+    case(1):{forward(l_rand_spd, l_rand_tim);Serial.println("random num 1, forward"); break;}
+    case(2):{forward(l_rand_spd, l_rand_tim);Serial.println("random num 1, forward"); break;}
+    case(3):{leftturn(l_rand_spd, l_rand_tim);Serial.println("random num 2, leftturn"); break;} 
+    case(4):{rightturn(l_rand_spd, l_rand_tim);Serial.println("random num 3, rightturn"); break;}   
+    case(5):{backward(l_rand_spd, l_rand_tim);Serial.println("random num 4, backward"); break;}
+    case(6):{backward(l_rand_spd, l_rand_tim);Serial.println("random num 4, backward"); break;}
+  }
+}
+void go_home(char p_command){   //p for paremeter
+  switch(p_command){
+    case('L'): { leftturn(g_turn_spd, g_turn_tim); nowstop(g_tim); break;}
+    case('R'): { rightturn(g_turn_spd, g_turn_tim); nowstop(g_tim); break;}
+    case('F'): { forward(g_spd, g_tim); break;}
+    case('B'): { backward(g_spd, g_tim); break;}
+    case('S'): { nowstop(g_tim); break;}
+    //case('S'): { nowstop(tim); break;}
+    //default: Serial.println("waiting for command");
+  }
 }
 
+//// sensor
 bool is_danger(){
   if(sonar.ping_cm()<10)
     return 1;
   else
     return 0;
 }
-
 bool is_alarm(){
   int l_human_sensor = analogRead(HUMAN_PIN);
-  if (l_human_sensor = 1023) return 1;
+  if (l_human_sensor == 1023) return 1;
   else return 0;
 }
-
+//// communication
+char get_command(){
+  //char l_command = ' '; // l for local value
+  if (Serial.available() > 0){
+    command = Serial.read(); //p for paremeter
+    Serial.print("Serial read: ");
+    Serial.println(command);
+  }
+  return command;
+}
+//// debug
 void print_sonar(){
     Serial.print("Ping: ");
     Serial.print(sonar.ping_cm());
     Serial.println("cm");
 }
-
 void print_human_sensor(){
   Serial.print("Human sensor: ");
   Serial.println(analogRead(HUMAN_PIN));
 }
 
-char get_command(){
-  char l_command; // l for local value
-  if (Serial.available() > 0){
-    l_command = Serial.read(); //p for paremeter
-  }
-  return l_command;
-}
   
-void go_home(char p_command){   //p for paremeter
-  switch(p_command){
-    case('L'): { leftturn(spd, tim); break;}
-    case('R'): { rightturn(spd, tim); break;}
-    case('F'): { forward(spd, tim); break;}
-    case('B'): { backward(spd, tim); break;}
-    case('S'): { nowstop(tim); break;}
-    default: Serial.println("waiting for command");
-  }
-}
 
 
--- src/xiaochetest/xiaochetest.ino
+++ src/xiaochetest/xiaochetest.ino
@@ -7,11 +7,30 @@ void setup() {
   setup_motors;
 }
 void loop() {
-  print_sonar();
-  print_human_sensor();
-  command =get_command();
-  go_home(command); 
-  delay(1000);
+  
+  //alarm = is_alarm(); // if alarm return 1; else return 0
+  alarm = 1;
+  
+  while (!alarm){  // alarm == 0, no alarm.
+    //print_sonar();
+    //print_human_sensor();
+    //Serial.println("No alarm, run randomly");
+    random_run();
+    //alarm = is_alarm(); // update alarm.
+    delay(1000); 
+  }
+  while (alarm){ // alarm == 1, alarm!
+    //print_sonar();
+    //print_human_sensor();
+    //Serial.println("Alarm! go home puppy!");
+    command = get_command();
+    Serial.println(command);
+    Serial.println(command == 'F');
+    go_home(command);
+    delay(100); // delay 100 ms
+    //alarm = is_alarm(); // update alarm
+  }
+  //delay(1000);
 }
 
 
